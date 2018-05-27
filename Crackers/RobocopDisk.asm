5bd600d5899ca990b5339511e96b04b70a461b2b
muckypaws <muckypaws@users.noreply.github.com>
2018-05-27 22:28:56 +0100

    Create RobocopDisk.asm

---
+++ Crackers/RobocopDisk.asm
@@ -0,0 +1,339 @@
+;
+         ORG  #a000                     ; Alternative DDFDC Commands
+         ENT  $                         ; Version 9.89
+         DI   
+         LD   bc,#fa7e
+         LD   a,1
+         OUT  (c),a
+         INC  a
+         INC  a
+         LD   bc,0
+loop
+         DJNZ loop
+         DEC  c
+         JR   nz,loop
+         DEC  a
+         JR   nz,loop
+         LD   de,0
+         CALL movtrak
+         LD   d,5
+         CALL movtrak
+         CALL readid
+         LD   a,(diskid+5)
+         CP   #23
+         JP   z,#a200
+         LD   hl,#c000
+         LD   b,#10
+         LD   c,1
+         LD   d,3
+         CALL readin
+         LD   hl,#100
+         LD   c,2
+         LD   d,8
+         LD   b,#19
+         LD   a,e
+         LD   (#3f22),a
+         JP   #3f66
+readin
+         LD   e,0
+         CALL readsect
+         INC  c
+         LD   a,c
+         CP   6
+         JR   nz,readin1
+         INC  d
+         LD   c,1
+readin1
+         DJNZ readin
+         RET  
+         RET  
+readsect                                ; ENTRY HL=BUFFER E=DRIVE D=TRK C=SECTR
+         PUSH af
+         PUSH bc
+         PUSH de
+         PUSH hl                        ; Preserve Registers
+         CALL movtrak                   ; Move To Approprite Track
+         LD   a,d
+         LD   (readcomd+3),a
+         LD   a,c
+         LD   (readcomd+5),a
+         LD   (readcomd+7),a
+         LD   de,readcomd
+         CALL ddfdccom                  ; Set Up Command
+         POP  hl
+         CALL ddfdcexc                  ; Get Bytes From EXECUTION PHASE
+         PUSH hl
+         LD   hl,readcomr
+         CALL ddfdcres                  ; Get Result From Read Command
+         POP  hl
+         POP  de
+         POP  bc
+         POP  af
+         RET  
+readid
+         PUSH af                        ; Entry E=drive 0=A 1=B
+         PUSH bc
+         PUSH de
+         LD   a,e
+         LD   (Read_ID+2),a             ; Store Drive
+         LD   hl,diskid
+         PUSH hl
+         LD   de,Read_ID
+         CALL ddfdccom
+         CALL ddfdcres
+         POP  hl                        ; Exit Cond. HL=Disk Id, All Reg. Pres.
+         POP  de
+         POP  bc
+         POP  af
+         RET  
+;
+;                        **** Move Drive Head To Track T ****
+;
+movtrak                                 ; On Entry D = Destination Track
+         PUSH af
+         PUSH bc
+         PUSH de
+         PUSH hl                        ; Preserve Registers
+         LD   a,e
+         OR   #20
+         LD   (traky),a
+         LD   a,d                       ; A = Track
+         OR   a                         ; Is Destination Track 0
+         JR   z,trak0                   ; If So Then Trak0
+         CALL seek                      ; Give DDFDC SEEK Command
+movtrak1                                ; Use This Since No DMA
+         CALL senseint                  ; Has Drive Reached Destination ?
+         LD   hl,traky
+         LD   a,(sense)
+         CP   (hl)
+         JR   nz,movtrak1
+movtrake                                ; Quit Routine
+         POP  hl
+         POP  de
+         POP  bc
+         POP  af
+         RET                            ; Preserved Registers
+trak0
+         CALL recalib                   ; Recalibrate (Move To Track 0)
+trak01
+         CALL senseds                   ; Call SENSE DRIVE STATUS
+         LD   a,(hl)
+         AND  %10000                    ; Has Drive Head Reached TRK 0
+         JR   z,trak01                  ; If Not Loop
+         JR   movtrake                  ; Restore Regs & Quit
+senseds
+         LD   a,(drive)
+         LD   (SenseCde+2),a
+         LD   de,SenseCde
+         CALL ddfdccom
+         LD   hl,sendst                 ; Pointer To Put Resultant Data
+         CALL ddfdcres
+         DEC  hl                        ; On Exit HL=Address Of Status Reg. 3
+         RET                            ; Quit
+;
+;                  **** RECALIBRATE DRIVE HEAD (TRACK 0) ****
+;
+recalib                                 ; Recalibrate
+         LD   de,RECALCOM
+         LD   a,(drive)
+         LD   (RECALCOM+2),a
+         JP   ddfdccom
+;
+;                       **** SENSE INTERUPT STATUS ****
+;
+senseint                                ; Sense Interupt Status
+         PUSH de                        ; Preserve DE
+         LD   de,SENSEINT
+         CALL ddfdccom
+         LD   hl,sense
+         CALL ddfdcres                  ; Call & Quit
+         POP  de
+         RET  
+;
+;                   **** SEEK COMMAND MOVE TRACK HEAD ****
+;
+seek                                    ; SEEK Entry D=Track
+         PUSH de
+         LD   (Seek_DT+3),a
+         LD   a,(drive)
+         LD   (Seek_DT+2),a
+         LD   de,Seek_DT
+         CALL ddfdccom
+         POP  de
+         RET  
+;
+;                        **** DDFDC COMMAND PHASE ****
+;
+ddfdccom                                ; DDFDC Command Phase
+         LD   bc,#fb7e
+         PUSH bc
+         LD   a,(de)                    ; Get Number Of Parameters
+ddfdc
+         PUSH af                        ; Preserve Counter
+         INC  de
+ddfdc1                                  ; Is Drive Ready To Accept Command ?
+         IN   a,(c)
+         ADD  a,a
+         JR   nc,ddfdc1
+         JP   m,ddfdc1                  ; If Not Then Wait
+         LD   a,(de)
+         INC  c
+         OUT  (c),a                     ; Give DDFDC Command @ Port #FB7F
+         DEC  c
+         LD   a,5
+ddfdcp                                  ; Wait 13 uS
+         DEC  a
+         JR   nz,ddfdcp
+         POP  af
+         DEC  a
+         JR   nz,ddfdc
+         POP  bc                        ; On Return BC=#FB7E
+         RET                            ; Quit
+;
+ddfdcexc                                ; DDFDC Execution Phase - DATA IN
+         LD   a,(drive)
+         OR   #20
+         LD   (ddfdexc2-1),a
+ddfdcexd
+         LD   bc,#fb7e
+         IN   a,(c)
+         CP   #c0
+         JR   c,ddfdexc1
+ddfdexc0
+         INC  c                         ; Point To #FB7F - DATA REGISTER
+         IN   a,(c)                     ; Get byte from port
+         LD   (hl),a                    ; Store it
+         DEC  c                         ; Restore Port To Main Status Reg.
+         INC  hl                        ; HL+1
+ddfdexc1
+         IN   a,(c)
+         JP   p,ddfdexc1                ; Drive Not Finished Output So Wait
+         AND  #20                       ; Main Status Reg=Execution Phase Start
+ddfdexc2
+         JR   nz,ddfdexc0               ; If Not Finished Loop ddfdexc
+         RET                            ; Else Quit
+ddfdcres                                ; DDFDC Result Phase
+         LD   a,(drive)
+         OR   #10
+         LD   (ddfdresq-1),a
+ddfdcret
+         IN   a,(c)
+         CP   #c0                       ; Is DDFDC Ready ?
+         JR   c,ddfdcret                ; If Not Wait
+         INC  c
+         IN   a,(c)                     ; Get Byte From DATA REG
+         LD   (hl),a                    ; Store it
+         DEC  c                         ; Restore Data Reg.
+         INC  hl                        ; HL+1
+         LD   a,5
+ddfdresp                                ; Wait 13 uS
+         DEC  a
+         JR   nz,ddfdresp
+         IN   a,(c)
+         AND  #10                       ; Has Results Finished ?
+ddfdresq
+         JR   nz,ddfdcret               ; If Not Loop ddfdcres
+         RET                            ; Quit 
+;
+;                  **** DATA AREA BUFFERS, POINTERS Etc. ****
+;
+sense                                   ; Data from RESULT PHASE OF SNSE INT ST
+         DEFB 0                         ; ST0
+         DEFB 0                         ; Present Track Number
+;
+;             **** BYTE FOR SENSE DRIVE STATUS RESULT PHASE ****
+;
+sendst   DEFB 0                         ; Status Register 3
+SenseCde
+         DEFB 2                         ; 2 Parameters
+         DEFB 4                         ; Code For SENSE DRIVE STATUS
+         DEFB 0                         ; Drive
+;
+;                   **** COMMAND DATA FOR SEEK ****
+;
+Seek_DT                                 ; Seek Codes For Command
+         DEFB 3                         ; 3 Parameters
+         DEFB 15                        ; Command For Seek
+         DEFB 0                         ; Drive
+         DEFB 0                         ; Destination Track
+;
+;            **** COMMAND DATA FOR SENSE INTERUPT STATUS ****
+;
+SENSEINT DEFB 1                         ; One Parameter
+         DEFB 8                         ; Command Code For SENSE INTERUPT STATE
+;
+;                  **** COMMAND DATA FOR READ ID ****
+;
+Read_ID
+         DEFB 2                         ; 2 Parameters
+         DEFB #4a                       ; Command Number
+         DEFB 0                         ; Drive
+;
+;                 **** RESULTANT DATA FROM READ ID ****
+;
+diskid                                  ; DISK ID Resultant Data
+         DEFB 0                         ; ST0
+         DEFB 0                         ; ST1
+         DEFB 0                         ; ST2
+         DEFB 0                         ; Track
+         DEFB 0                         ; Head
+         DEFB 0                         ; Sector
+         DEFB 0                         ; Sector Size = 2 ^ N+7
+;
+;                 **** COMMAND DATA FOR RECALIBRATE ****
+;
+RECALCOM DEFB 2                         ; 2 Parameters
+         DEFB 7                         ; Command Code For RECALIBRATE
+         DEFB 0                         ; Which Drive
+;
+;             **** COMMAND CODE FOR DDFDC WRITE SECTOR ****
+;
+writcomd                                ; Write Command Data
+         DEFB 9                         ; 9 Parameters
+         DEFB #45                       ; WRITE DATA Command Alter #49 Del. Dta
+         DEFB 0                         ; Drive
+         DEFB 0                         ; Track
+         DEFB 0                         ; Head Number
+         DEFB 0                         ; Sector To Read
+         DEFB 0                         ; Number Of Data Bytes Per Sector
+         DEFB 0                         ; End Of Track
+         DEFB 0                         ; Gap #3 - Generally #2a
+         DEFB #ff                       ; Data Length - Sect Size < 256
+;
+;         **** WRITE DATA RESULTANT BYTES FROM RESULT PHASE ****
+;
+writcomr                                ; Write Command Result Table
+         DEFB 0                         ; Status Register 0
+         DEFB 0                         ; Status Register 1
+         DEFB 0                         ; Status Register 2
+         DEFB 0                         ; Track Number
+         DEFB 0                         ; Head Number
+         DEFB 0                         ; Sector Number
+         DEFB 0                         ; Sector Size
+;
+;                 **** COMMAND DATA FOR READ SECTOR ****
+;
+readcomd                                ; Read Command Data
+         DEFB 9                         ; 9 Parameters
+         DEFB #4c                       ; READ DATA Command Alter To #4C Del.
+         DEFB 0                         ; Drive
+         DEFB 3                         ; Track
+         DEFB 0                         ; Head Number
+         DEFB 1                         ; Sector To Read
+         DEFB 3                         ; Number Of Data Bytes Per Sector
+         DEFB 1                         ; End Of Track
+         DEFB 1                         ; Gap #3 - Generally #2a
+         DEFB #ff                       ; Data Length - Sect Size < 256
+;
+;        **** READ DATA RESULTANT BYTES FROM RESULT PHASE ****
+;
+readcomr                                ; Read Command Result Table
+         DEFB 0                         ; Status Register 0
+         DEFB 0                         ; Status Register 1
+         DEFB 0                         ; Status Register 2
+         DEFB 0                         ; Track Number
+         DEFB 0                         ; Head Number
+         DEFB 0                         ; Sector Number
+         DEFB 0                         ; Sector Size
+drive    DEFB 0
+traky    DEFB 0
